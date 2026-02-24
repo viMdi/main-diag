99b5e61fbdee07b7b8b4b436c79ebd9ca6953df7
viMdi <bckmvv@hotmail.com>
2026-02-25 02:32:18 +0300

    Initial commit: диагностика D-Link

---
+++ cfg_example.py
@@ -0,0 +1,15 @@
+# Конфигурационный файл
+# 1. Скопируйте этот файл как cfg.py
+# 2. Замените значения ниже на актуальные для вашей среды
+
+DB_CONFIG = {
+    "host": "your_db_host",
+    "database": "your_db_name",
+    "user": "your_db_user",
+    "password": "your_db_password",
+    "charset": "cp1251",
+}
+
+# Учетные данные для подключения к коммутаторам
+SWITCH_USERNAME = "your_switch_username"
+SWITCH_PASSWORD = "your_switch_password"
